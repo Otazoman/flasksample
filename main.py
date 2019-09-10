cb2aaeccdbb91f05c9559f5e1e2ffe47d4381785
Otazoman <ujimasa@hotmail.com>
2019-09-10 18:48:45 +0900

    20190910 create and update API

---
+++ main.py
@@ -0,0 +1,54 @@
+# coding: UTF-8
+import json
+from flask import Flask, jsonify, make_response, request, Response
+from flask_api import status
+from google.cloud import datastore
+import datetime
+
+app = Flask(__name__)
+client = datastore.Client()
+
+@app.route('/api/createdata', methods=['POST'])
+def create_user():
+  try:
+    data = request.data.decode('utf-8')
+    data = json.loads(str(data))
+    key = client.key("Task2")
+    entity = datastore.Entity(key)
+    entity.update(data)
+    client.put(entity)
+    response = jsonify({'status':'200','message': 'success'})
+    return response
+  except Exception as e:
+    result = error_handler(e)
+    return result
+
+@app.route('/api/searchdata', methods=['GET'])
+def search_data():
+  try:
+    data = request.data.decode('utf-8')
+    data = json.loads(str(data))
+    k= [i for i in data.keys()]
+    v = [ j for j in data.values()]
+    query = client.query(kind="Task2")
+    query.add_filter(k[0], "=", v[0] )
+    result = list(query.fetch())
+    return jsonify(result)
+  except Exception as e:
+    return error_handler(e)
+
+@app.errorhandler(400)
+@app.errorhandler(404)
+@app.errorhandler(500)
+def error_handler(error):
+    response = jsonify({
+                          "error": {
+                          "type": error.name,
+                          "message": error.description
+                          }
+                      })
+    return response, error.code
+
+if __name__ == '__main__':
+  app.debug=True
+  app.run(host='0.0.0.0')
